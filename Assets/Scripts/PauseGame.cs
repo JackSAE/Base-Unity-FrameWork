174b1e19465fd1cf305b0ded360b89390a831b93
JackSAE <1005787@student.sae.edu.au>
2016-12-13 23:31:35 +1000

    Added More features and basic scripts and assets

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PauseGame : MonoBehaviour {
+
+    public GameObject canvas;
+    public Transform player;
+
+	// Update is called once per frame
+	void Update () {
+        PauseTheGame();
+
+    }
+
+    public void PauseTheGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                //Disable player movement
+                player.GetComponent<playerController>().enabled = false;
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                //Re-enable player movement
+                player.GetComponent<playerController>().enabled = false;
+            }
+        }
+    }
+}
